9b530a983ac439deae6bfd1ab331befa190addcc
AnthonyTB <anthonylol@protonmail.com>
2020-09-02 10:35:03 -0700

    completed project template

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+import "./App.css";
+import { RouteComponentProps } from "@reach/router";
+
+const App: FunctionComponent<RouteComponentProps> = () => {
+  return <div className="App"></div>;
+};
+
+export default App;
